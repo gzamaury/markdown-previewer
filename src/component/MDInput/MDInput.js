348fad4ee6d57cbc790f73c52d557fdbf9f8ad6f
Amaury Gz <gzamaury@hotmail.com>
2022-09-10 02:22:01 +0000

    Add: small fixes

--- src/component/MDInput/MDInput.js
+++ src/component/MDInput/MDInput.js
@@ -59,16 +59,20 @@ MDInput.propTypes = {
 };
 
 const textDefaultInput = `# Markdown Basic
+#
 ## Writing and formatting syntax
+
 ### Headings:
-- # Heading level 1
-- ## Heading level 2
-- ### Heading level 3
-- #### Heading level 4
-- ##### Heading level 5
-- ###### Heading level 6
+
+  - # Heading level 1
+  - ## Heading level 2
+  - ### Heading level 3
+  - #### Heading level 4
+  - ##### Heading level 5
+  - ###### Heading level 6
 
 ## Styling text
+
 Style | Syntax | Example
 ----- | ------ | -------
 Bold  | \`** **\` or \`__ __\` | **This is bold text**
@@ -80,64 +84,71 @@ Subscript | \`<sub> </sub>\` | <sub>This is a subscript text</sub>
 Superscript | \`<sup> </sup>\` | <sup>This is a superscript text</sup>
 
 ## Quoting text
+
 Text that is not a quote
 > Text that is a quote
 
 ## Quoting code
+
 You can call out code or a command within a sentence with single backticks (\`\`\`\` \` \`\`\`\`), e.g. the \`console.log()\` method outputs a message to the web console.
 
 To format code or text into its own distinct block, use triple backticks (\`\`\`\` \`\`\` \`\`\`\`).
 
 \`\`\`js
-// JS - destructuring assignment
-let a, b;
-[a, b] = [10, 20];
+  // JS - destructuring assignment
+  let a, b;
+  [a, b] = [10, 20];
 
-console.log(a);
-// expected output: 10
+  console.log(a);
+  // expected output: 10
 
-console.log(b);
-// expected output: 20
+  console.log(b);
+  // expected output: 20
 \`\`\`
 
 ## Links
+
 You can create an inline link by wrapping link text in brackets \`[ ]\`, and then wrapping the URL in parentheses \`( )\`.
 
 These examples are based on [Basic writing and formatting syntax](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax).
 
 ## Images
+
 You can display an image by adding \`!\` and wrapping the alt text in \`[ ]\`. Then wrap the link for the image in parentheses \`()\`.
 
 ![This is an image](https://myoctocat.com/assets/images/base-octocat.svg)
+
 ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
 
 ## Lists
+
 You can make an unordered list by preceding one or more lines of text with \`-\` or \`*\`.
 
-- George Washington
-- John Adams
-- Thomas Jefferson
+  - George Washington
+  - John Adams
+  - Thomas Jefferson
 
 To order your list, precede each line with a number.
 
-1. James Madison
-2. James Monroe
-3. John Quincy Adams
+  1. James Madison
+  2. James Monroe
+  3. John Quincy Adams
 
 ## Nested Lists
+
 You can create a nested list by indenting one or more list items below another item.
 
-1. First list item
-   - First nested list item
-     - Second nested list item
+  1. First list item
+    - First nested list item
+      - Second nested list item
 
 ## Task lists
 
 To create a task list, preface list items with a hyphen and space followed by \`[ ]\`. To mark a task as complete, use \`[x]\`.
 
-- [x] Task checked
-- [ ] Pending task
-- [ ] Another pending task
+  - [x] Task checked
+  - [ ] Pending task
+  - [ ] Another pending task
 
 `;
 
